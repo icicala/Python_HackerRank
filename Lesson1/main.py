8b7a5bc724c360c48a5c2004f333cce2910fe7bb
icicala <icicala.ubb@gmail.com>
2022-09-27 16:45:10 +0200

    If Else

--- Lesson1/main.py
+++ Lesson1/main.py
@@ -1,8 +1,26 @@
+'''
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+'''
+
 
 def main():
-    pass
+    n = int(input().strip())
 
-
+    if (n % 2 == 0):
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+    else:
+        print("Weird")
 
 if __name__ == "__main__":
     main()
